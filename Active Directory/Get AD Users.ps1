88a7c40f48d76a221eb3d58f5e7eeedba6b0473a
Scott Holzberger <sholzberger@rbcgroup.com.au>
2023-06-02 14:06:37 +1000

    New files & minor updates

--- Active Directory/Get AD Users.ps1
+++ Active Directory/Get AD Users.ps1
@@ -1,1 +1,6 @@
-asdasda
+Y
+Suspend-MoveRequest -Identity "wreynolds@rbcgroup.com.au"
+Suspend-MoveRequest -Identity "dklass@rbcgroup.com.au"
+Resume-MoveRequest -Identity "mmanton@rbcgroup.com.au"
+Resume-MoveRequest -Identity "dklass@rbcgroup.com.au"
+Resume-MoveRequest -Identity "wreynolds@rbcgroup.com.au"
---
+++ O365/New O365 Support User.ps1
@@ -0,0 +1,35 @@
+﻿
+#Required Modules
+
+#Install-Module AzureAD
+#Install-Module MSOnline
+#Install-module Microsoft.Graph.Identity.Signins
+
+Import-Module  AzureAD
+
+Connect-AzureAD
+
+$domain = "rbc.support@rbcgroupau.onmicrosoft.com"
+
+$PasswordProfile=New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.ForceChangePasswordNextLogin = $false
+$PasswordProfile.Password="3Rv0y1q39/chsy"
+New-AzureADUser -DisplayName "RBC Support Admin" -GivenName "RBC Support" -SurName "Admin" -UserPrincipalName $domain -PasswordProfile $PasswordProfile -MailNickName rbc.support -AccountEnabled $true
+
+Connect-MsolService
+
+Set-AzureADUser -ObjectId $domain -OtherMails @("ip2me@rbcgroup.com.au") -Mobile "+61447782853" -TelephoneNumber "+61447782853" 
+Add-MsolRoleMember -RoleMemberEmailAddress $domain -RoleName "Exchange Recipient Administrator"
+Add-MsolRoleMember -RoleMemberEmailAddress $domain -RoleName "Global Reader"
+
+
+    $mf= New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+    $mf.RelyingParty = "*"
+    $mfa = @($mf)
+    Set-MsolUser -UserPrincipalName $domain -StrongAuthenticationRequirements $mfa
+
+    $m1.IsDefault = $true
+    $m1.MethodType="OneWaySMS"
+  
+set-msoluser -Userprincipalname rbc.support@rbcgroupau.onmicrosoft.com -StrongAuthenticationMethods $m1
+New-MgUserAuthenticationPhoneMethod -UserId rbc.support@rbcgroupau.onmicrosoft.com -phoneType "mobile" -phoneNumber +61447782853
