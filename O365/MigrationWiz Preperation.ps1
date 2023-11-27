e24ae81e6ead7edd0d5d0f3928ea76a69bf8c912
Scott Holzberger <sholzberger@rbcgroup.com.au>
2023-11-27 14:40:46 +1000

    asd

--- O365/MigrationWiz Preperation.ps1
+++ O365/MigrationWiz Preperation.ps1
@@ -1,4 +1,6 @@
 
-$user = Read-Host "Please provide username that will be used for migration..."
+$user =  "admin@NexusCarecomau.onmicrosoft.com" #Read-Host "Please provide username that will be used for migration..."
 
-Get-Mailbox -ResultSize Unlimited | Add-MailboxPermission -AccessRights FullAccess -Automapping $false -User admin@NexusCarecomau.onmicrosoft.com
+Get-Mailbox -ResultSize Unlimited | Add-MailboxPermission -AccessRights FullAccess -Automapping $false -User $user
+Enable-OrganizationCustomization
+New-ManagementRoleAssignment -Role "ApplicationImpersonation" -User $user
